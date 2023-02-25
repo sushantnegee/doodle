f491449eb901f0b3fb227e9f9f125e9d6f047990
sushantnegee <sushantnegee@gmail.com>
2023-02-25 17:25:20 +0530

    solved

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -11,12 +11,12 @@ const userSchema = mongoose.Schema({
     timestamps:true,
 });
 
-userSchema.method.matchPassword = async function (enteredPassword){
+userSchema.methods.matchPassword = async function (enteredPassword){
     return await bcrypt.compare(enteredPassword,this.password);
 }
 
 userSchema.pre('save',async function (next){
-    if(!this.modified){
+    if(!this.isModified){
         next();
     }
     
