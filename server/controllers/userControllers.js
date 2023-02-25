8896c2b1c07970f3bab2b64b043af7de8c8e33ad
sushantnegee <sushantnegee@gmail.com>
2023-02-25 17:06:12 +0530

    bycrypt added but not encrypted the password

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -42,7 +42,7 @@ const authUser = asyncHandler(async(req,res)=>{
   const {email,password} = req.body;
   const user = await User.findOne({email});
 
-  if(user){
+  if(user && (await User.matchPassword(password))){
     res.json({
       _id:user._id,
         name:user.name,
@@ -53,4 +53,4 @@ const authUser = asyncHandler(async(req,res)=>{
   }
 })
 
-module.exports = {registerUser}
+module.exports = {registerUser,authUser}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
     name:{type:String,required:true},
@@ -9,6 +10,19 @@ const userSchema = mongoose.Schema({
 },{
     timestamps:true,
 });
+
+userSchema.method.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
+
+userSchema.pre('save',async function (next){
+    if(!this.modified){
+        next();
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+})
 
 const User = mongoose.model("User",userSchema);
 
