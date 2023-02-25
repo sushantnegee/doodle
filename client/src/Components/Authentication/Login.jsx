3b6dd88de401bca9411182bb64c5d18143a7126e
sushantnegee <sushantnegee@gmail.com>
2023-02-25 22:59:30 +0530

    login done but some error occured

--- client/src/Components/Authentication/Login.jsx
+++ client/src/Components/Authentication/Login.jsx
@@ -1,18 +1,67 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement,useToast,VStack } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 const Login = () => {
     const [show, setShow] = useState(false)
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const navigate = useNavigate();
     const handleClick = ()=>{
         setShow(!show);
     }
 
-    const submiHandler = ()=>{
-
+    const submiHandler = async ()=>{
+    setLoading(true);
+    if(!email || !password){
+        toast({
+            title: 'Please Fill all Fields',
+            status: 'warning',
+            duration: 4000,
+            isClosable: true,
+            position:"bottom"
+          })
+          setLoading(false);
+          return;
     }
+    try{
+        const config = {
+            headers:{
+                "Content-type":"application/json"
+            }
+        };
+
+        const data = await axios.post("/api/user/login",{email,password},config);
+
+        toast({
+            title: 'Login Successfull',
+            status: 'success',
+            duration: 4000,
+            isClosable: true,
+            position:"bottom"
+          })
+
+          localStorage.setItem('userInfo',JSON.stringify(data));
+          setLoading(false);
+        //   history.push("/chats");
+          navigate("/chats");
+    }catch(error){
+        toast({
+            title: 'Error Occured',
+            description:error.response.data.message,
+            status: 'success',
+            duration: 4000,
+            isClosable: true,
+            position:"bottom"
+          })
+          console.log("error:",error)
+          setLoading(false); 
+    }
+    }
+    console.log(loading)
   return <VStack spacing={'5px'}>
     <FormControl isRequired>
         <FormLabel>Email</FormLabel>
@@ -29,7 +78,7 @@ const Login = () => {
         </InputRightElement>
         </InputGroup>
     </FormControl>
-    <Button colorScheme={'blue'} width="100%" style={{marginTop:15}} onClick={submiHandler}>
+    <Button colorScheme={'blue'} width="100%" style={{marginTop:15}} isLoading={loading} onClick={submiHandler}>
         Login
     </Button>
   </VStack>;
