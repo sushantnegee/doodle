712c0ca479cf148882f203e0dd5d95df0c1757eb
sushantnegee <sushantnegee@gmail.com>
2023-03-06 13:50:22 +0530

    search authorization

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const User = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
+
+const protect = asyncHandler(async (req,res,next)=>{
+    let token ;
+    if(
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ){
+        try{
+            token = req.headers.authorization.split(" ")[1];
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decode.id).select("-password");
+            console.log(req.user);
+            next();
+        }catch(error){
+            res.status(401);
+            throw new Error("Not Authorized, Token Failed")
+        }
+    }
+    
+    if(!token){
+        res.status(401);
+        throw new Error("Not Authorized, No Token")
+    }
+})
+
+module.exports = {protect};
