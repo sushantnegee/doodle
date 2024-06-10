26e4e11e3aa8277945c88c2681319e2d4bc5a9f6
sushantnegee <sushantnegee@gmail.com>
2024-06-10 22:10:53 +0530

    fix: backend api link changed

--- client/src/Config/ApiLink.js
+++ client/src/Config/ApiLink.js
@@ -1,2 +1,2 @@
-export const API_LINK = 'https://doodle-talk.up.railway.app';
+export const API_LINK = 'https://doodle-talk-backend.onrender.com';
 // export const API_LINK = 'http://localhost:5000';
